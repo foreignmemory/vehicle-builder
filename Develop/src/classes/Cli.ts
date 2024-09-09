7ee668f6e2db2a57500e663f37481f14aed9f5ee
Tristan Perrotti <perrotti.tristan@gmail.com>
2024-09-09 14:24:15 -0400

    fixed syntax errors

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -187,7 +187,8 @@ class Cli {
           parseInt(answers.year),
           parseInt(answers.weight),
           parseInt(answers.topSpeed),
-          []
+          [],
+          parseInt(answers.towingCapacity)
         );
 
         this.vehicles.push(truck);
@@ -409,10 +410,9 @@ class Cli {
             }
             
           }
-
         // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
-        else if (answers.action === 'Select or create another vehicle') {
+        } else if (answers.action === 'Select or create another vehicle') {
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
           this.startCli();
           return;
